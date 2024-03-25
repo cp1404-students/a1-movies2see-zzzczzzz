242fc49d71371bb2b1324cc723b05991ebfced3f
yunseo choi <160458927+zzzczzzz@users.noreply.github.com>
2024-03-25 14:40:12 +0800

    Update assignment1.py
    basic stucture built

--- assignment1.py
+++ assignment1.py
@@ -1,14 +1,41 @@
 """
-Name:
-Date started:
-GitHub URL (of this assignment):
+Name: yunseo choi
+Date started: march 21 2024
+GitHub URL (of this assignment): https://github.com/cp1404-students/a1-movies2see-zzzczzzz/tree/feedback
 Remember to NEVER make this assignment repo public
 """
 
 
+MENU = """ Menu:
+D - Display movies
+A - Add new movie
+W - Watch a movie
+Q - Quit
+"""
+
+
 def main():
-    """..."""
+    print("Welcome to Movie Manager!")
+    movies = load_movies()
+    choice = input("Enter your choice: ").upper()
+    while choice != "Q":
+        print(MENU)
+
+        if choice == "D":
+            display_movies(movies)
+        elif choice == "A":
+            add_movie(movies)
+        elif choice == "W":
+            watch_movie(movies)
+        else:
+            print("Invalid input")
+        choice = input("Enter your choice: ").upper()
+
+    save_movies_to_csv(movies)
+    print("Movies saved")
 
 
-if __name__ == '__main__':
-    main()
+def load_movies:
+    in_file = open('movies.csv', 'r')
+
+main()
